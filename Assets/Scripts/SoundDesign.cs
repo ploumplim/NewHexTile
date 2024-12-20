d477f3c6c07fd8c51bbb3ec1a7331141754503a2
EthanAzoulay <147728860+EthanAzoulay@users.noreply.github.com>
2024-12-20 15:46:18 +0100

    FMOD Integration

---
+++ Assets/Scripts/SoundDesign.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundDesign : MonoBehaviour
+{
+    public void MouseOverTiles()
+    {
+        // Play sound effect when the mouse goes over a tile
+    }
+
+    public void Click()
+    {
+        // Play sound effect when the left mouse button is pressed
+    }
+
+    public void DestroyTiles()
+    {
+        // Play sound effect when a tile is destroyed
+    }
+
+    public void Fusion()
+    {
+        // Play sound effect when a tile is fusionned
+    }
+
+    public void Improve()
+    {
+        // Play sound effect when a tile improve life time
+    }
+
+    public void Bomba()
+    {
+        // Play sound effect when the bomba is used
+    }
+
+}
