652008fa9643af7ab2325fc23e525771958e5880
ploumplim <moreaubapt@gmail.com>
2024-12-21 10:21:18 +0100

    Ui script with fonction to modify TextColor

--- Assets/Scripts/UiChanger.cs
+++ Assets/Scripts/UiChanger.cs
@@ -1,22 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UiChanger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     
-    public static  void ChangeTextColor( TextMesh textMesh, Color color)
+    public static  void ChangeTextColor( TextMeshPro textMesh, Color color)
     {
         textMesh.color = color;
     }
