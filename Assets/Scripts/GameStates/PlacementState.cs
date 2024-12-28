a1b41fe3589b28134790c7a2e880393fe13fbee3
ploumplim <moreaubapt@gmail.com>
2024-12-28 17:36:06 +0100

    Ajout de L'ui d'affichage de prédépot de tile

--- Assets/Scripts/GameStates/PlacementState.cs
+++ Assets/Scripts/GameStates/PlacementState.cs
@@ -7,6 +7,8 @@ using UnityEngine.UI;
 
 public class PlacementState : States
 {
+
+    private HexagonTile previousHexTile;
     public override void Enter()
     {
         // Generate the next tile to be placed on the board by updating the next tile state to the first element in the future tile list.
@@ -18,8 +20,7 @@ public class PlacementState : States
     
     public override void Tick()
     {
-        if (Input.GetMouseButtonDown(0)) // Detect left mouse button click
-        {
+        
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
@@ -27,58 +28,133 @@ public class PlacementState : States
             {
                 // If the ray hits a hexagon tile, we want to apply the next tile state to it.
                 HexagonTile hexTile = hit.collider.GetComponent<HexagonTile>();
+                
+                
+               
+                
                 if (hexTile != null)
                 {
-                    if (hexTile.currentTileState == HexagonTile.TileStates.LegalTile)
-                    {
-                        // If godmode is enabled, we want to apply the tile state that is selected in the godHUD
-                        if (GM.GODMODE)
+                    if (Input.GetMouseButtonDown(0)) // Detect left mouse button click
+                    {    
+                        if (hexTile.currentTileState == HexagonTile.TileStates.LegalTile)
                         {
-                            int activeTileIndex = GM.godModeToggleScript.activeTile;
-                            switch (activeTileIndex)
+                            // If godmode is enabled, we want to apply the tile state that is selected in the godHUD
+                            if (GM.GODMODE)
                             {
-                                case 0:
-                                    NextTileCreate(hexTile);
-                                    break;
-                                case 1:
-                                    hexTile.TileStateChange(HexagonTile.TileStates.GreenTile);
-                                    break;
-                                case 2:
-                                    hexTile.TileStateChange(HexagonTile.TileStates.BlueTile);
-                                    break;
-                                case 3:
-                                    hexTile.TileStateChange(HexagonTile.TileStates.RedTile);
-                                    break;
-                                case 4:
-                                    hexTile.TileStateChange(HexagonTile.TileStates.DestroyerTile);
-                                    break;
-                                case 5:
-                                    hexTile.TileStateChange(HexagonTile.TileStates.PakkuTile);
-                                    break;
-                                
-                                default:
-                                    Debug.LogError("Invalid active tile index: " + activeTileIndex);
-                                    break;
-                                
+                                int activeTileIndex = GM.godModeToggleScript.activeTile;
+                                switch (activeTileIndex)
+                                {
+                                    case 0:
+                                        NextTileCreate(hexTile);
+                                        break;
+                                    case 1:
+                                        hexTile.TileStateChange(HexagonTile.TileStates.GreenTile);
+                                        break;
+                                    case 2:
+                                        hexTile.TileStateChange(HexagonTile.TileStates.BlueTile);
+                                        break;
+                                    case 3:
+                                        hexTile.TileStateChange(HexagonTile.TileStates.RedTile);
+                                        break;
+                                    case 4:
+                                        hexTile.TileStateChange(HexagonTile.TileStates.DestroyerTile);
+                                        break;
+                                    case 5:
+                                        hexTile.TileStateChange(HexagonTile.TileStates.PakkuTile);
+                                        break;
+                                    
+                                    default:
+                                        Debug.LogError("Invalid active tile index: " + activeTileIndex);
+                                        break;
+                                    
+                                } 
                             } 
-                        } 
-                        else // If godmode is disabled, we want to apply the next tile state to the hexagon tile
-                        {
-                            NextTileCreate(hexTile);
-                        }
+                            else // If godmode is disabled, we want to apply the next tile state to the hexagon tile
+                            {
+                                NextTileCreate(hexTile);
+                            }
                         
                         
-                        GM.ChangeState(GM.GetComponent<EffectState>());
+                            GM.ChangeState(GM.GetComponent<EffectState>());
 
                         
+                        }
                     }
                 }
+                
+                
+                if (hexTile.currentTileState == HexagonTile.TileStates.LegalTile)
+                {
+
+                    if (hexTile!= previousHexTile)
+                    {
+                        
+                        if (previousHexTile != null)
+                        {
+                            if (previousHexTile.currentTileState == HexagonTile.TileStates.LegalTile)
+                            {
+                                foreach (var VARIABLE in previousHexTile.tileVisuals)
+                                {
+                                    VARIABLE.SetActive(false);   
+                                }
+                                previousHexTile.tileVisuals[1].SetActive(true); 
+                            }
+                            
+                        }
+                        previousHexTile = hexTile;
+                    }
+                    
+                    
+                    switch (GM.futureTileStateList[0])
+                    {
+                        case HexagonTile.TileStates.GreenTile:
+                            foreach (var visualsTile in hexTile.tileVisuals)
+                            {
+                                visualsTile.SetActive(false);
+                            }
+                            hexTile.tileVisuals[3].SetActive(true);
+                            break;
+                        case HexagonTile.TileStates.BlueTile:
+                            foreach (var visualsTile in hexTile.tileVisuals)
+                            {
+                                visualsTile.SetActive(false);
+                            }
+                            hexTile.tileVisuals[4].SetActive(true);
+                            
+                            break;
+                        case HexagonTile.TileStates.RedTile:
+                            
+                            foreach (var visualsTile in hexTile.tileVisuals)
+                            {
+                                visualsTile.SetActive(false);
+                            }
+                            hexTile.tileVisuals[5].SetActive(true);
+                            break;
+                        case HexagonTile.TileStates.DestroyerTile:
+                            foreach (var visualsTile in hexTile.tileVisuals)
+                            {
+                                visualsTile.SetActive(false);
+                            }
+                            hexTile.tileVisuals[10].SetActive(true);
+                            break;
+                        case HexagonTile.TileStates.PakkuTile:
+                            foreach (var visualsTile in hexTile.tileVisuals)
+                            {
+                                visualsTile.SetActive(false);
+                            }
+                            hexTile.tileVisuals[11].SetActive(true);
+                            break;
+                    }
+                }
+                
+                
+                
+                
             }
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            GM.SwapIncomingTiles(); 
-        }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                GM.SwapIncomingTiles(); 
+            }
     }
     
    
