3894b812b9e37f88e8b3837ff7c80910b97f63f0
ploumplim <moreaubapt@gmail.com>
2025-01-23 15:01:02 +0100

    Correction Bug tool Prog

--- Assets/Scripts/HexagonGrid.cs
+++ Assets/Scripts/HexagonGrid.cs
@@ -17,26 +17,25 @@ public class HexagonGrid : MonoBehaviour
 
     public void InitGrid()
     {
-        gridHeight = LevelData.selectedLevelExotic.gridX;
-        gridWidth = LevelData.selectedLevelExotic.gridY;
+        gridHeight = LevelData.selectedLevelExotic.gridY;
+        gridWidth = LevelData.selectedLevelExotic.gridX;
         TileInstances = new HexagonTile[gridWidth, gridHeight];
-        for (int y = 0; y < gridWidth; y++)
+        for (int y = 0; y < gridHeight; y++)
         {
-            for (int x = 0; x < gridHeight; x++)
+            for (int x = 0; x < gridWidth; x++)
             {
-                GameObject[,] tileGameObjects = new GameObject[gridWidth, gridHeight];
                 float xPosition = y % 2 == 0 ? x * tileScale : x * tileScale + tileScale / 2.0f;
                 float yPosition = y * 0.9f * tileScale;
                 Vector3 position = new Vector3(xPosition, 0.0f, yPosition);
                 Vector3 scale = Vector3.one * tileScale;
 
-                tileGameObjects[x, y] = Instantiate(hexagonTilePrefab, position, Quaternion.identity, transform);
-                tileGameObjects[x, y].transform.localScale = scale; 
-                
-                TileInstances[x, y] = tileGameObjects[x, y].GetComponent<HexagonTile>();
-                
+                GameObject tileGameObject = Instantiate(hexagonTilePrefab, position, Quaternion.identity, transform);
+                tileGameObject.transform.localScale = scale;
+
+                TileInstances[x, y] = tileGameObject.GetComponent<HexagonTile>();
+
                 HexagonTile tile = TileInstances[x, y];
-                
+
                 if (tile != null)
                 {
                     tile.InitializeTile();
