409a51206e43170d6ff9c82171226bb73c7ea5f9
Nathan jeger <njegerpariente@myges.fr>
2025-02-01 12:52:49 +0100

    BUILD SIYTENANCE BONUS LEVEL CLASSIQUE

--- Assets/ARCHIVE/HexStates(DEPRECATED)/animationScrpt.cs
+++ Assets/ARCHIVE/HexStates(DEPRECATED)/animationScrpt.cs
@@ -7,12 +7,53 @@ using UnityEngine;
 public class animationScrpt : MonoBehaviour
 {
     public SkinnedMeshRenderer SkinnedMeshRenderer;
-    HexagonTile HexagonTile;
+    public HexagonTile HexagonTile;
     // Start is called before the first frame update
     private void Awake()
     {
-        HexagonTile = FindAnyObjectByType<HexagonTile>();
+        
 
-        SkinnedMeshRenderer.SetBlendShapeWeight(0, Mathf.Lerp(0, 100, HexagonTile.lifeTime));
+        
+    }
+    public void Update()
+    {
+        if (HexagonTile.lifeTime >=15)
+        {
+            SkinnedMeshRenderer.SetBlendShapeWeight(0, 10);
+        }
+
+        if (HexagonTile.lifeTime == 10)
+        {
+            SkinnedMeshRenderer.SetBlendShapeWeight(0, 30);
+        }
+        if (HexagonTile.lifeTime == 8)
+        {
+            SkinnedMeshRenderer.SetBlendShapeWeight(0, 45);
+        }
+        if (HexagonTile.lifeTime == 6)
+        {
+            SkinnedMeshRenderer.SetBlendShapeWeight(0, 66);
+        }
+        if (HexagonTile.lifeTime == 5)
+        {
+            SkinnedMeshRenderer.SetBlendShapeWeight(0, 70);
+        }
+        if (HexagonTile.lifeTime == 4)
+        {
+            SkinnedMeshRenderer.SetBlendShapeWeight(0, 80);
+        }
+        if (HexagonTile.lifeTime == 3)
+        {
+            SkinnedMeshRenderer.SetBlendShapeWeight(0, 90);
+        }
+        if (HexagonTile.lifeTime == 2)
+        {
+            SkinnedMeshRenderer.SetBlendShapeWeight(0, 95);
+        }
+        if (HexagonTile.lifeTime == 1)
+        {
+            SkinnedMeshRenderer.SetBlendShapeWeight(0, 98);
+        }
+
     }
 }
